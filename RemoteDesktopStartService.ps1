0ece9f58c69fa2412eba8882ebd077652c51bf72
Juanjo Cerezuela <Juanjo.Cerezuela@asos.com>
2017-03-13 18:10:08 +0000

    Adding Script to remote desktop into a machine and start a service

---
+++ RemoteDesktopStartService.ps1
@@ -0,0 +1,31 @@
+﻿Param(
+        [Parameter(Mandatory = $true,      Position = 0,  HelpMessage = 'Name of your machine')]
+        [String]$ComputerName,
+        [Parameter(Mandatory = $true,      Position = 1,  HelpMessage = 'Name of your service')]
+        [String]$ServiceName
+)
+
+$service = Get-Service -Name $ServiceName  -ComputerName $ComputerName
+
+if($service)
+{
+    Write-Host 'Status of' $ServiceName 'for machine ' $ComputerName 'is' $service.Status
+
+    if($service.Status -eq 'Running')
+    {
+      Write-Host 'Is already running'
+    }
+    else
+    {
+
+        Write-Host 'Starting service...'
+        $service.Start()
+        Start-Sleep -s 2
+        $service = Get-Service -Name $ServiceName  -ComputerName $ComputerName
+        Write-Host $service.Status 'is now' $service.Status
+    }
+}
+else
+{
+    Write-Host 'The service doesnt exist in this machine'
+}
